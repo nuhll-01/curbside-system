4437d4595b25aaaafff44c46d017817046ba3a33
ToastyyX <erick.cool321@icloud.com>
2025-08-20 16:41:51 -0500

    Imported 'java.sql' package

--- src/CurbsidePickupDriver.java
+++ src/CurbsidePickupDriver.java
@@ -1,3 +1,5 @@
+import java.sql.SQLException;
+
 /**
  * Author: Erick Chavez
  * Description: Simulating a text-based store curbside system
@@ -8,7 +10,7 @@ public class CurbsidePickupDriver {
      * starting point of the program
      * @param args string array of  list of arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         CurbsideSystem sys = new CurbsideSystem();
         DatabaseManager.initializeDatabase();
         sys.start();

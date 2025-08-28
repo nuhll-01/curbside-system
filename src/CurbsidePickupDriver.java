9ab783948b12a8a6223f912388b873e0796d2daa
ToastyyX <erick.cool321@icloud.com>
2025-08-28 14:33:14 -0500

    Corrected Comment Indentation

--- src/CurbsidePickupDriver.java
+++ src/CurbsidePickupDriver.java
@@ -8,7 +8,7 @@ public class CurbsidePickupDriver {
 
     /**
      * starting point of the program
-     * @param args string array of  list of arguments
+     * @param args string array of list of arguments
      */
     public static void main(String[] args) throws SQLException {
         CurbsideSystem sys = new CurbsideSystem();
